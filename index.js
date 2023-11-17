b9f13898547330839de8f517c1e5a638a7fa2886
Marc Charles <marccharles243@gmail.com>
2023-11-16 20:54:11 -0500

    adds random freelancer

--- index.js
+++ index.js
@@ -1,54 +1,88 @@
-const freelancers = [
-    { name: "Dr. Slice", price: 25, occupation: "gardener" },
-    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "teacher" },
-    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    { name: "Prof. Spark", price: 76, occupation: "programmer" },
-    { name: "Dr. Wire", price: 47, occupation: "teacher" },
-    { name: "Prof. Goose", price: 72, occupation: "driver" },
-];
-
-const abc = [
-    { name: "Carol", price: 70, occupation: "programmer" },
-    { name: "Bob", price: 50, occupation: "teacher" },
-    { name: "Alice", price: 30, occupation: "writer" },
-]
-
+//Create a state
+const state = {
+    average: 10,
+    abc : [
+        { name: "Carol", price: 70, occupation: "programmer" },
+        { name: "Bob", price: 50, occupation: "teacher" },
+        { name: "Alice", price: 30, occupation: "writer" },
+    ],
+    freelancers : [
+        { name: "Dr. Slice", price: 25, occupation: "gardener" },
+        { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+        { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+        { name: "Prof. Prism", price: 81, occupation: "teacher" },
+        { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+        { name: "Prof. Spark", price: 76, occupation: "programmer" },
+        { name: "Dr. Wire", price: 47, occupation: "teacher" },
+        { name: "Prof. Goose", price: 72, occupation: "driver" },
+    ]
+}
 //Html Development
 const body = document.querySelector("body");
 const p = document.querySelector("p");
 p.style.fontSize = "1.25rem";
-const sumPrice = abc.reduce((acc, currentValue) =>  acc + currentValue.price,0);
-const avgPrice = sumPrice / abc.length;
-const priceLabel = document.querySelector("#price");
-
-priceLabel.textContent = avgPrice.toString();
-
-const table = document.createElement("table");
-table.createTHead()
-table.style.width = "100px";
-table.style.border = "1px solid black";
-
-//Create Table Header
-let tHead = table.createTHead();
-let row = tHead.insertRow();
-for (let key of Object.keys(abc[0])) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
+function randFreelancer() {
+    const rand = Math.floor(Math.random() * state.freelancers.length);
+    return state.freelancers[rand];
+}
+function getAvgPrice() {
+    const sumPrice = state.abc.reduce((acc, currentValue) =>  acc + currentValue.price,0);
+    const avgPrice = sumPrice / state.abc.length;
+    return avgPrice.toString();
 }
 
-//Generate Table Rows with details
-for (let element of abc) {
-    let row = table.insertRow();
-    for (let key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
+function updateAvgPrice(){
+    const priceLabel = document.querySelector("#price");
+    priceLabel.textContent = getAvgPrice();
+    return priceLabel;
+}
+
+// function clearTable() {
+//     while (table.rows.length) {
+//         table.
+//     }
+// }
+const table = document.createElement("table");
+
+//Create Table Header
+function createTbHeader() {
+    let tHead = table.createTHead();
+    let row = tHead.insertRow();
+    for (let key of Object.keys(state.abc[0])) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
     }
 }
 
+//Generate Table Rows with details
+function addRows() {
+    for (let element of state.abc) {
+        let row = table.insertRow();
+        for (let key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+}
+
+function addRandFlRows(){
+    let row = table.insertRow();
+    let randLancer = randFreelancer();
+    for (let key in randLancer) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(randLancer[key]);
+        cell.appendChild(text);
+    }
+}
+function render() {
+    createTbHeader();
+    addRows();
+    addRandFlRows();
+    updateAvgPrice();
+    body.append(table);
+}
+render();
 //Add table to HTML
-body.append(table);
